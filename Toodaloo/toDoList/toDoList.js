6f055e5ddef5395a0b88e1b822de7e0b20d0cd2d
JaySuar12 <114695232+JaySuar12@users.noreply.github.com>
2022-11-06 20:44:26 -0800

    attempted user data permanent storage
    Tested storing user data in a text file to keep data after the extension is refreshed it didn't work.

--- Toodaloo/toDoList/toDoList.js
+++ Toodaloo/toDoList/toDoList.js
@@ -1,3 +1,27 @@
+/*
+I tried to store user data on a text file as to keep data when user
+refreches the page and was not able to with vanillia js
+if we want to store data we will have to use servers
+looked into free servers for begginer and aws seems like the best option
+*/
+const fs = require('fs/promises');
+
+async function example() {
+  window.alert('It almost ran');
+  try {
+    window.alert('It ran');
+
+    const content = 'Some content!';
+    await fs.writeFile('../toDoList/data.txt', content);
+    window.alert('It worked');
+  } catch (err) {
+    window.alert('It failed');
+    console.log(err);
+  }
+}
+example();
+//window.alert('Hello world');
+
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -38,7 +62,6 @@ function myFunction() {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  window.alert('pressed');
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
